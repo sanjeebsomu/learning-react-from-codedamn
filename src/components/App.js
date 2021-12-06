b5caf89e89517597147c28702d1a9bdb99c6b7c3
sanjeebsomu <sanjeebgochhayat1999@gmail.com>
2021-12-06 18:01:51 +0530

    To do app(with better understanding)

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,32 @@
-import React,{ useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 
 function App() {
-  const [todos, setTodos] = useState(['Add Your Task From Above Input Field'])
-  const [extract, setExtract] = useState()//extract from input field
-
-  function createTodo(){
-    setTodos(oldTodos =>{
-      return[...oldTodos, extract]
+  const [todos, setTodos] = useState([ //this hook is for displaying the todos list, will use submit when add btn will be clicked
+    'sanjeeb',                         //this value will be stored inside todos variable
+    'gochhayat',
+    'somu',
+    'bhubaneswar'
+  ])
+  const [task, setTask] = useState('')//this hook is for getting the value inside the input field
+  function addTodo(){
+    setTodos(oldTodos => {
+      return [...oldTodos, task]
+      
     })
   }
 
   return (
     <>
-    <h1>Best To-Do App Ever</h1>
-    <input type="text" value={extract} onChange={e =>{
-      setExtract(e.target.value)
-    }} />
-    <button onClick={createTodo} >Create TODO</button>
-    <ul>
-      {todos.map(todo =>{
+      <h1>This is the best TODO App ever</h1>
+      <input value={task} onChange={e =>{     //value and onchange is necessery
+        setTask(e.target.value)               //settask re se input value ku grab kariki task variable ku patha hela then settodos re sei task varibble ku use karikki ta value ku display karagala
+      }}/>
+      <button onClick={addTodo} >Add</button>
+      <ul>{todos.map(todo => {
         return <li>{todo}</li>
-      })}
-    </ul>
+      })}</ul>
     </>
   );
 }
