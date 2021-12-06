f9270c15d85a2cd3c793843f6af7505fef2dda28
sanjeebsomu <sanjeebgochhayat1999@gmail.com>
2021-12-06 18:59:26 +0530

    empty the input field after click add button, and adding Enter key functionality(onkeydown)

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,45 @@
-import React, { useState } from 'react';
-import './App.css';
-
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [todos, setTodos] = useState([ //this hook is for displaying the todos list, will use submit when add btn will be clicked
-    'sanjeeb',                         //this value will be stored inside todos variable
-    'gochhayat',
-    'somu',
-    'bhubaneswar'
-  ])
-  const [task, setTask] = useState('')//this hook is for getting the value inside the input field
-  function addTodo(){
-    setTodos(oldTodos => {
-      return [...oldTodos, task]
-      
-    })
+  const [todos, setTodos] = useState([
+    //this hook is for displaying the todos list, will use submit when add btn will be clicked
+    "sanjeeb", //this value will be stored inside todos variable
+    "gochhayat",
+    "somu",
+    "bhubaneswar",
+  ]);
+  const [task, setTask] = useState(""); //this hook is for getting the value inside the input field
+
+  function addTodo() {
+    setTask(""); //this will set the input value blank
+    setTodos((oldTodos) => {
+      return [...oldTodos, task]; //old todos with new texts
+    });
   }
 
+  function tryToCheckForEnterKey(e){
+    if (e.keyCode === 13) {
+      addTodo();
+    }
+  }
   return (
     <>
       <h1>This is the best TODO App ever</h1>
-      <input value={task} onChange={e =>{     //value and onchange is necessery
-        setTask(e.target.value)               //settask re se input value ku grab kariki task variable ku patha hela then settodos re sei task varibble ku use karikki ta value ku display karagala
-      }}/>
-      <button onClick={addTodo} >Add</button>
-      <ul>{todos.map(todo => {
-        return <li>{todo}</li>
-      })}</ul>
+      <input
+        onKeyDown={tryToCheckForEnterKey}
+        value={task}
+        onChange={(e) => {
+          //value and onchange is necessery
+          setTask(e.target.value); //settask re se input value ku grab kariki task variable ku patha hela then settodos re sei task varibble ku use karikki ta value ku display karagala
+        }}
+      />
+      <button onClick={addTodo}>Add</button>
+      <ul>
+        {todos.map((todo) => {
+          return <li>{todo}</li>;
+        })}
+      </ul>
     </>
   );
 }
