0cdc74b082374ec84df90b9ecb9849e68ca8f124
sanjeebsomu <sanjeebgochhayat1999@gmail.com>
2021-12-06 09:49:08 +0530

    Use state functional arguement

--- src/components/App.js
+++ src/components/App.js
@@ -6,15 +6,27 @@ import Header from './Header';
 function App() {
   const [counter, setCounter] = useState(0)
 
-  function btnClickedFn() {
-    setCounter(1);
+  function increase() {
+    // setCounter(counter + 1);           //do this when we do not depend on the older value
+    /*setCounter(function(oldCounterVal){   //do this when we have to depend on the previous(older value)
+      return oldCounterVal + 1;
+    })*/
+    //or one liner
+    setCounter(oldCounterVal => oldCounterVal + 1)//read this as "set counter function oldecounterval return oldcounterval + 1"
+  }
+  function decrease() {
+    // setCounter(counter - 1);           //do this when we do not depend on the older value
+    setCounter(function(oldCounterVal){   //do this when we have to depend on the previous(older value)
+      return oldCounterVal - 1;
+    })
   }
 
   return (
     <>
     <Header />
     <h1>Counter: {counter}</h1>
-    <button onClick={btnClickedFn} >Increase</button>
+    <button onClick={increase} >Increase</button>
+    <button onClick={decrease} >Decrease</button>
     </>
   );
 }
